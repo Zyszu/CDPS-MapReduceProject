90ea340b0f94b928fe7ea42d4e6bf34b3bb3e1bd
Zyszu <zyszu2001@gmail.com>
2025-11-19 18:57:45 +0100

    init commit

---
+++ MasterNode/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+using System.Text;
+
+// Get your active network interface
+var iface = NetworkInterface.GetAllNetworkInterfaces()
+    .First(n => n.OperationalStatus == OperationalStatus.Up &&
+                n.NetworkInterfaceType != NetworkInterfaceType.Loopback);
+
+var props = iface.GetIPProperties();
+var unicast = props.UnicastAddresses
+    .First(u => u.Address.AddressFamily == AddressFamily.InterNetwork);
+
+var localIP = unicast.Address;
+var mask = unicast.IPv4Mask;
+
+// Compute broadcast address
+byte[] ipBytes = localIP.GetAddressBytes();
+byte[] maskBytes = mask.GetAddressBytes();
+byte[] broadcastBytes = new byte[4];
+
+for (int i = 0; i < 4; i++)
+    broadcastBytes[i] = (byte)(ipBytes[i] | (maskBytes[i] ^ 255));
+
+var broadcastIP = new IPAddress(broadcastBytes);
+
+Console.WriteLine("Broadcast IP = " + broadcastIP);
+
+// Bind to interface
+var udp = new UdpClient(new IPEndPoint(localIP, 0));
+udp.EnableBroadcast = true;
+
+var data = Encoding.UTF8.GetBytes("Hello LAN!");
+
+while (true)
+{
+    udp.Send(data, data.Length, new IPEndPoint(broadcastIP, 5000));
+    Console.WriteLine("Broadcast sent!");
+    
+    await Task.Delay(1000);
+}
---
+++ WorkerNode/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Listening for UDP messages on port 5000...");
+
+        var udp = new UdpClient(5000);  // Bind to port 5000
+        var sender = new IPEndPoint(IPAddress.Any, 0);
+
+        while (true)
+        {
+            var data = udp.Receive(ref sender);
+            string message = Encoding.UTF8.GetString(data);
+
+            if (message == "Hello LAN!")
+            {
+                Console.WriteLine($"[{DateTime.Now}] Got it from {sender.Address}");
+            }
+        }
+    }
+}
